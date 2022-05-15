b4955fe62e367a89b00ecc00fa028e3f57e9739d
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2022-05-15 13:42:52 +0600

    Nevber complete and all component route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './LogInPg/LogIn';
+import AboutMe from './Pages/AboutMe';
+import Blog from './Pages/Blog';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Navber from './Shared/Navber';
 
 function App() {
   return (
     <div>
-      My own app
+     <Navber></Navber>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/services' element={<Services></Services>}></Route>
+       <Route path='/blog' element={<Blog></Blog>}></Route>
+       <Route path='/aboutme' element={<AboutMe></AboutMe>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+     </Routes>
     </div>
   );
 }
--- src/Shared/Navber.js
+++ src/Shared/Navber.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PrivateTutorlogo from "../assets/Private Tutor.png";
 
 const Navber = () => {
     return (
         <div>
-            This is Navber page
+            <div className="navbar bg-primary text-primary-content flex justify-around">
+                <div >
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">
+                    <img src={PrivateTutorlogo} alt="PrivateTutorlogo" className="rounded-xl h-14" /></Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-6 text-lg font-bold">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/aboutme">About Me</Link></li>
+                    <li><Link to="/login">Log In</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
